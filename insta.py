9b6f70d8ffdaf769fa95ccc41f89075f1410ff47
unknown <vickyvinish98@gmail.com>
2022-09-06 12:42:03 +0530

    changed variables

--- insta.py
+++ insta.py
@@ -22,15 +22,17 @@ for follower in profile.get_followers():
 follower_name = 'username'
 
 for follower in profile.get_followers():
-    if follower.username == 'follower_name':
+    if follower.username == follower_name:
         # Creating and adding follower usernames to a list
         follow_list = []
         count = 0
         for followee in follower.get_followees():
             follow_list.append(followee.username)
             # Writing username to a text file
-            with open('srikanth_follow_list.txt', 'a+') as f:
+            with open(follower_name + '_follows_list.txt', 'a+') as f:
                 f.write(follow_list[count])
                 f.write('\n')
                 count += 1
-        print(f'Total number of people Srikanth follows is {len(follow_list)}')
+        print(f'Total number of people {follower_name} follows is {len(follow_list)}')
+
+
