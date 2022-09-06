9d86a4ee1ae9af53574432c84c49e09faabfcdb7
unknown <vickyvinish98@gmail.com>
2022-09-06 12:23:07 +0530

    generalized the file

--- insta.py
+++ insta.py
@@ -5,29 +5,30 @@ from instaloader import Profile
 instance = instaloader.Instaloader()
 
 #login or load session
-user = 'vickyvinish98@gmail.com'
-password = '3blue1brown'
+user = 'user_mail'
+password = 'password'
 
 instance.login(user, password)
 
 # Fetching fullname of the profile:
-profile = Profile.from_username(instance.context, username='vikkyvigneshhh')
-# print(profile.full_name)
+profile = Profile.from_username(instance.context, username='username')# username of the profile
+print(f"The full name of the profile is {profile.full_name}")
 
-# Getting the followers of the profile:
-# for followee in profile.get_followers():
-#     print(followee.username)
+# Returns all the username of the profile's followers:
+for follower in profile.get_followers():
+    print(follower.username)
 
 # Getting followees of a sepcific user_account:
 for follower in profile.get_followers():
     if follower.username == 'srikanthmadanagopal':
+        # Creating and adding follower usernames to a list
         follow_list = []
         count = 0
         for followee in follower.get_followees():
             follow_list.append(followee.username)
+            # Writing username to a text file
             with open('srikanth_follow_list.txt', 'a+') as f:
                 f.write(follow_list[count])
                 f.write('\n')
                 count += 1
         print(f'Total number of people Srikanth follows is {len(follow_list)}')
-        print("Mission Accomplished")
