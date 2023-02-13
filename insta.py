3f9e9a28e0304260fece2f85df4fdf6c292d1425
unknown <vickyvinish98@gmail.com>
2023-02-13 11:46:48 +0530

    made several changes

--- insta.py
+++ insta.py
@@ -5,34 +5,54 @@ from instaloader import Profile
 instance = instaloader.Instaloader()
 
 #login or load session
-user = 'user_mail'
-password = 'password'
+user = 'vikkyvigneshhh' # Replace your username here.
+instance.interactive_login(user) # Enter the password in the terminal to login
 
-instance.login(user, password)
-
-# Fetching fullname of the profile:
-profile = Profile.from_username(instance.context, username='username')# username of the profile
-print(f"The full name of the profile is {profile.full_name}")
-
-# Returns all the username of the profile's followers:
-for follower in profile.get_followers():
-    print(follower.username)
-
-# Getting followees of a sepcific user_account:
-follower_name = 'username'
-
-for follower in profile.get_followers():
-    if follower.username == follower_name:
-        # Creating and adding follower usernames to a list
-        follow_list = []
-        count = 0
-        for followee in follower.get_followees():
-            follow_list.append(followee.username)
-            # Writing username to a text file
-            with open(follower_name + '_follows_list.txt', 'a+') as f:
-                f.write(follow_list[count])
-                f.write('\n')
-                count += 1
-        print(f'Total number of people {follower_name} follows is {len(follow_list)}')
+# Fetching profile of the user:
+profile = Profile.from_username(instance.context, user) # username of the profile
 
 
+# Returns the profile's followers as a list:
+def get_followers( profile ):
+    return [ follower.username for follower in profile.get_followers()]
+
+# Returns the profile's followees as a list:
+def get_followees( profile ):
+    return [ followee.usernmae for followee in profile.get_followees() ]
+
+
+# Getting followings of a sepcific user_account:
+def  get_followings_of_specific_account( profile, followee_name ):
+    for followee in profile.get_followees():
+        if followee.username == followee_name:
+            following_list = []
+            count = 0
+            # Loop through and add followings to a text file.
+            for followee in followee.get_followees():
+                following_list.append(followee.username)
+                with open(followee_name + '_followings_list.txt', 'a+') as f:
+                    f.write(following_list[count])
+                    f.write('\n')
+                    count += 1
+
+
+# working with saved_posts: (Function needs to be checked and updated)
+def saved_posts( profile ):
+    return profile.get_saved_posts()
+
+if __name__ == "__main__":
+
+    print(f" Hi {profile.full_name}!! ")
+    print("\n")
+
+    print(f" You follow { profile.followees } profiles")
+    # print(get_followees( profile ))  // Uncomment this line if you want the list of following.
+    
+    print(f" { profile.followers } profiles follow you")
+    # print(get_followers( profile )) // Uncomment this line if you want the list of followers.
+
+    # To get followings of a sepcific user_account you follow:
+    followee_name = 'srikanthmadanagopal' # Replace the with the required username 
+    get_followings_of_specific_account( profile, followee_name )
+
+    print(saved_posts( profile ))
